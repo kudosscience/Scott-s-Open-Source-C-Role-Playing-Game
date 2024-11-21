9c2fe893c75037ff0185a3446960fbc4cb2b6fdd
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2024-11-21 16:24:38 +0000

    Add Location.cs and image to main screen

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,6 +9,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -18,6 +19,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 100000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
