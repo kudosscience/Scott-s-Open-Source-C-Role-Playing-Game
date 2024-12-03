10013fa652c63a1cc9bcfccebee69ad65c45783d
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2024-12-03 18:59:41 +0000

    Refactor – Encapsulate LivingEntity Properties (Hit Points and Gold)

--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 namespace Engine.Models
 {
     public abstract class LivingEntity : BaseNotificationClass
     {
+        #region Properties
         private string _name;
         private int _currentHitPoints;
         private int _maximumHitPoints;
@@ -12,7 +14,7 @@ namespace Engine.Models
         public string Name
         {
             get { return _name; }
-            set
+            private set
             {
                 _name = value;
                 OnPropertyChanged(nameof(Name));
@@ -21,7 +23,7 @@ namespace Engine.Models
         public int CurrentHitPoints
         {
             get { return _currentHitPoints; }
-            set
+            private set
             {
                 _currentHitPoints = value;
                 OnPropertyChanged(nameof(CurrentHitPoints));
@@ -30,7 +32,7 @@ namespace Engine.Models
         public int MaximumHitPoints
         {
             get { return _maximumHitPoints; }
-            set
+            private set
             {
                 _maximumHitPoints = value;
                 OnPropertyChanged(nameof(MaximumHitPoints));
@@ -39,7 +41,7 @@ namespace Engine.Models
         public int Gold
         {
             get { return _gold; }
-            set
+            private set
             {
                 _gold = value;
                 OnPropertyChanged(nameof(Gold));
@@ -49,10 +51,50 @@ namespace Engine.Models
         public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; set; }
         public List<GameItem> Weapons =>
             Inventory.Where(i => i is Weapon).ToList();
-        protected LivingEntity()
+        public bool IsDead => CurrentHitPoints <= 0;
+        #endregion
+        public event EventHandler OnKilled;
+        protected LivingEntity(string name, int maximumHitPoints, int currentHitPoints, int gold)
         {
+            Name = name;
+            MaximumHitPoints = maximumHitPoints;
+            CurrentHitPoints = currentHitPoints;
+            Gold = gold;
             Inventory = new ObservableCollection<GameItem>();
             GroupedInventory = new ObservableCollection<GroupedInventoryItem>();
+        }
+        public void TakeDamage(int hitPointsOfDamage)
+        {
+            CurrentHitPoints -= hitPointsOfDamage;
+            if (IsDead)
+            {
+                CurrentHitPoints = 0;
+                RaiseOnKilledEvent();
+            }
+        }
+        public void Heal(int hitPointsToHeal)
+        {
+            CurrentHitPoints += hitPointsToHeal;
+            if (CurrentHitPoints > MaximumHitPoints)
+            {
+                CurrentHitPoints = MaximumHitPoints;
+            }
+        }
+        public void CompletelyHeal()
+        {
+            CurrentHitPoints = MaximumHitPoints;
+        }
+        public void ReceiveGold(int amountOfGold)
+        {
+            Gold += amountOfGold;
+        }
+        public void SpendGold(int amountOfGold)
+        {
+            if (amountOfGold > Gold)
+            {
+                throw new ArgumentOutOfRangeException($"{Name} only has {Gold} gold, and cannot spend {amountOfGold} gold");
+            }
+            Gold -= amountOfGold;
         }
         public void AddItemToInventory(GameItem item)
         {
@@ -89,5 +131,11 @@ namespace Engine.Models
             }
             OnPropertyChanged(nameof(Weapons));
         }
+        #region Private functions
+        private void RaiseOnKilledEvent()
+        {
+            OnKilled?.Invoke(this, new System.EventArgs());
+        }
+        #endregion
     }
 }
--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -8,18 +8,15 @@ namespace Engine.Models
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; private set; }
         public Monster(string name, string imageName,
-                       int maximumHitPoints, int hitPoints,
-                       int minimumDamage, int maxmumDamage,
-                       int rewardExperiencePoints, int rewardGold)
+                       int maximumHitPoints, int currentHitPoints,
+                       int minimumDamage, int maximumDamage,
+                       int rewardExperiencePoints, int gold) :
+            base(name, maximumHitPoints, currentHitPoints, gold)
         {
-            Name = name;
             ImageName = $"/Engine;component/Images/Monsters/{imageName}";
-            MaximumHitPoints = maximumHitPoints;
-            CurrentHitPoints = hitPoints;
             MinimumDamage = minimumDamage;
-            MaximumDamage = maxmumDamage;
-            RewardExperiencePoints = rewardExperiencePoints;
-            Gold = rewardGold;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints
         }
     }
 }
